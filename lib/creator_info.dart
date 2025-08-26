c178200564fb70106ab97e7facd1ef0c9b3a7a1a
nithish25kumar <nithish25may2005@gmail.com>
2025-08-26 12:42:07 +0530

    feat: userprofile created

--- lib/creator_info.dart
+++ lib/creator_info.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class CreatorInfo extends StatelessWidget {
-  const CreatorInfo({super.key});
+class Creatorpage extends StatefulWidget {
+  const Creatorpage({super.key});
 
   @override
+  State<Creatorpage> createState() => _CreatorpageState();
+}
+
+class _CreatorpageState extends State<Creatorpage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back_ios_new),
+          ),
+          backgroundColor: Color(0xff42C83C),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
+          ),
+          title: Text(
+            'Creator Info',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25,
+              letterSpacing: 5,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              CircleAvatar(
+                radius: 90,
+                backgroundImage: AssetImage('assets/images/img.png'),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'Nithishkumar K',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                'VIT Vellore',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  launchUrl(
+                    Uri.parse(
+                      'https://www.linkedin.com/in/nithish-kumar-9b29b3287/',
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      FontAwesomeIcons.linkedin,
+                      size: 20,
+                      color: Colors.blue,
+                    ),
+                    const SizedBox(width: 6),
+                    const Text(
+                      "LinkedIn",
+                      style: TextStyle(fontSize: 15, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Creator info',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: CreatorInfo(),
+      home: Creatorpage(),
     );
   }
 }
